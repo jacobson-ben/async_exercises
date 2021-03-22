5048d7cb172e982135e9a575531e7dad2a5c5ea0
Ben Jacobson <jacobson.ben11@gmail.com>
2021-03-22 15:12:59 -0400

    part 1 exercises

---
+++ app.js
@@ -0,0 +1,27 @@
+async function getNumbers(...args) {
+  resp = await axios.get(`http://numbersapi.com/${args}`);
+  data = resp.data
+  appendToPage(data)
+}
+
+function appendToPage(data) {
+  for(const key in data) {
+    $("ul").append(`<li>${key}: ${data[key]}</li>`)
+  }
+}
+
+async function getNumbers2(num) {
+  let baseURL = "http://numbersapi.com";
+
+  let numbers = await Promise.all([ 
+    axios.get(`${baseURL}/${num}?json`),
+    axios.get(`${baseURL}/${num}?json`),
+    axios.get(`${baseURL}/${num}?json`),
+    axios.get(`${baseURL}/${num}?json`)])
+  
+  console.log(numbers)
+
+  for(let i = 0; i < 4; i++) {
+    $("ul").append(`<li>${numbers[i].data.text}</li>`)
+  }
+}
